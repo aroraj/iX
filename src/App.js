04824cf9d489fa19b90c460af48270beede4d6cc
Jayti Arora <jaytiarora@Jaytis-MacBook-Pro.local>
2022-12-03 17:13:41 -0600

    final

--- src/App.js
+++ src/App.js
@@ -1,103 +1,54 @@
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap-icons/font/bootstrap-icons.css';
+import { Component } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 import firebase from './firebase/firebase';
 
-import './App.css';
-import { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
 
-import Table from './components/Table';
-import AddBook from './components/AddBook';
-import Book from './models/Book';
+import './App.css';
+
+import Home from './components/Home';
+import Login from './components/Login';
+import Register from './components/Register';
+import Navbar from './components/Navbar';
 
 class App extends Component {
-
   constructor(props) {
     super(props);
-    this.db = firebase.firestore();
-    /*let booksString = localStorage.getItem('books');
-    booksString = booksString ? booksString : '[]';
-    const books = JSON.parse(booksString);*/
 
-    this.state = { books: [] };
+    this.auth = firebase.auth();
+
+    this.state = {
+      user: null,
+      loading: true,
+    };
   }
 
-  async componentDidMount(){
-    this.fetchBooks();
-  }
-
-  async fetchBooks() {
-    try{
-      const snapshot = await this.db.collection('books').get();
-      const books = snapshot.docs.map(doc => Book.fromDocument(doc));
-      this.setState({ books: books });
-    } catch(err){
-      console.log(err);
-    }
-  }
-
-  async onBookCreated(book) {
-    console.log(book);
-    try {
-      const docRef = this.db.collection('books').doc();
-      await docRef.set({
-        title: book.title,
-        author: book.author,
-        isbn: book.isbn
-      });
-      book.id = docRef.id;
-      this.state.books.push(book);
-      this.setState({ books: this.state.books });
-    } catch(err){
-      console.log(err);
-    }
-  }
-
-  async onBookUpdated(book) {
-    try {
-      await this.db.collection('books').doc(book.id).update({
-        title: book.title,
-        author: book.author,
-        isbn: book.isbn
-      });
-      const updatedBookArr = this.state.books.map(t => t.id === book.id ? book : t);
-      this.setState({ books: updatedBookArr });
-    } catch(err){
-      console.log(err);
-    }
-  }
-
-  async onBookRemoved(bookId) {
-    try{
-      await this.db.collection('books').doc(bookId).delete();
-      const updatedBookArr = this.state.books.filter(book => book.id !== bookId);
-      this.setState({ books: updatedBookArr });
-    } catch(err) {
-      console.log(err);
-    }
+  componentDidMount() {
+    this.auth.onAuthStateChanged((user) => {
+      this.setState({ user: user, loading: false });
+    });
   }
 
   render() {
+    const { user, loading } = this.state;
+
     return (
-      <div className="container card mt-4 p-4">
+      <BrowserRouter>
 
-        <div className="text-left">
-          <h1>Add Book:</h1>
-        </div>
+      <Navbar user={user} />
 
-        <AddBook
-          createBook={(book) => this.onBookCreated(book)}
-        />
+      {loading ?
+          <div>Loading</div> :
+          <div>
+            <Route path='/' exact component={Home} />
+            <Route path='/login' component={Login} />
+            <Route path='/register' component={Register} />
+          </div>
+      }
 
-        <Table
-          books={this.state.books}
-          bookUpdated={(bookId) => this.onBookUpdated(bookId)}
-          bookRemoved={(bookId) => this.onBookRemoved(bookId)}
-        />
-        
-      </div>
-      
-      
+    </BrowserRouter>
     );
   }
 }
